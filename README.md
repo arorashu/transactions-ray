9c1c69b223ff67911b873198fc18eba3052d7c67
Shubham Arora <aroras@bu.edu>
2020-04-19 10:05:11 -0400

    1. Add car service and car db 2. Use ray.get() to get multiple futures 3. Reformat code according to PEP-8 (python formatting guide) 4. add system files in .gitignore

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Transactions in Ray
 
+## Instructions to run
+
+1. Ensure you have python 3.6+ installed
+
+2. Create python virtual env and install dependencies
+```
+    $ python3 -m venv .ray-env
+    $ source .ray-env/bin/activate
+    $ pip install -r requirements.txt
+```
+
+3. Run Trip Service
+```
+    $ python trip_service.py
+```
+
+
 ## Literature Review
 1. [Distributed Transactions](https://docs.google.com/presentation/d/1a7eXFUBe_PHn9NMTaNEqCsL-ENnUzifyI42KQlBceHo/edit#slide=id.p)
--- trip_service.py
+++ trip_service.py
@@ -1,25 +1,34 @@
 # Trip booking class
 
 import ray
-
 from flight_service import FlightBooker
+from car_service import CarBooker
+
 
 class TripBooker:
-  def __init__(self):
-    print("trip booking constructor")
-  
-  def getTripOptions(self, date, source , dest):
-    print(f"search Trip options for: date {date}, from: {source}, to:{dest}")
+    def __init__(self):
+        # print("trip booking constructor")
+        pass
+
+    def get_trip_options(self, date, source, dest):
+        print(f"search Trip options for: date {date}, from: {source}, to:{dest}")
 
 
 if __name__ == "__main__":
-  ray.init()
-  tripBooker = TripBooker()
-  tripBooker.getTripOptions("20200430", "BOS", "DEL")
-  flightBooker = FlightBooker.remote()
-  # flightBooker = FlightBooker()
-  # flightBooker.getTripOptions("20200430", "BOS", "DEL")
-  res = flightBooker.getTripOptions.remote("20200430", "BOS", "DEL")
-  resolvedResult = ray.get(res)
-  print("resolvedResult: " + str(resolvedResult))
+    ray.init()
+    date = "20200430"
+    source = "BOS"
+    destination = "DEL"
+    tripBooker = TripBooker()
+    tripBooker.get_trip_options(date, source, destination)
+    flight_booker = FlightBooker.remote()
+    flight_options = flight_booker.get_trip_options.remote("20200430", "BOS", "DEL")
+    car_booker = CarBooker.remote()
+    car_options = car_booker.get_trip_options.remote(date, source)
+
+    car_results, flight_results = ray.get([car_options, flight_options])
+
+    print("car results count: " + str(len(car_results)))
+    print("flight results count : " + str(len(flight_results)))
+
 
