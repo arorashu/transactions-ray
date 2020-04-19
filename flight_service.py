246fd08d89f98e2f28aa4b0741fc1449060979f4
Shubham Arora <aroras@bu.edu>
2020-04-19 08:40:57 -0400

    1. Add sample flight database 2. Setup basic search

--- flight_service.py
+++ flight_service.py
@@ -1,16 +1,37 @@
 # Trip booking class
 
+import ray
+import time
+import json
+
+@ray.remote
 class FlightBooker:
   def __init__(self):
     print("flight booking constructor")
+    
+    with open("./flight_db.json") as f:
+      self.flight_db = json.load(f)
+
+    # list of dict
+    # print(type(self.flight_db))
   
+  # @ray.remote spans a new process (new pid), anything that you print here
+  # does not show on the console, unless you do a ray.get() on the calling process
+  # 
+
   def getTripOptions(self, date, source , dest):
     print(f"search Flight options for: date {date}, from: {source}, to: {dest}")
+    flight_db = self.flight_db
+    
+    res = []
+    for flight_option in flight_db:
+      if flight_option['from'] == source and flight_option['to'] == dest and flight_option['date'] == date:
+        res.append(flight_option)
+
+    return res
 
 
 if __name__ == "__main__":
   tripBooker = FlightBooker()
   tripBooker.getTripOptions("20200416", "Boston", "Delhi")
 
-# export FlightBooker
-        
--- trip_service.py
+++ trip_service.py
@@ -1,5 +1,7 @@
 # Trip booking class
 
+import ray
+
 from flight_service import FlightBooker
 
 class TripBooker:
@@ -7,13 +9,17 @@ class TripBooker:
     print("trip booking constructor")
   
   def getTripOptions(self, date, source , dest):
-    print(f"search trip options for: date {date}, from: {source}, to:{dest}")
+    print(f"search Trip options for: date {date}, from: {source}, to:{dest}")
 
 
 if __name__ == "__main__":
+  ray.init()
   tripBooker = TripBooker()
-  tripBooker.getTripOptions("20200416", "Boston", "Delhi")
-  flightBooker = FlightBooker()
-  flightBooker.getTripOptions("20200416", "Boston", "Delhi")
+  tripBooker.getTripOptions("20200430", "BOS", "DEL")
+  flightBooker = FlightBooker.remote()
+  # flightBooker = FlightBooker()
+  # flightBooker.getTripOptions("20200430", "BOS", "DEL")
+  res = flightBooker.getTripOptions.remote("20200430", "BOS", "DEL")
+  resolvedResult = ray.get(res)
+  print("resolvedResult: " + str(resolvedResult))
 
-        
